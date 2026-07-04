8873968ecfc69870688e16ae451eb3b2a3eff75a
Riti <your_email@example.com>
2026-07-04 20:39:26 +0600

    Implemented common layout with header and footer

--- resources/views/layouts/apps.blade.php
+++ resources/views/layouts/apps.blade.php
@@ -3,7 +3,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Smart Canteen Pre-Order System</title>
+    <title>Smart Canteen Pre-Order System</title>=
 
     @vite(['resources/css/app.css', 'resources/js/app.js'])
 
---
+++ resources/views/layouts/header.blade.php
@@ -0,0 +1,50 @@
+<nav class="navbar navbar-expand-lg navbar-dark bg-success">
+    <div class="container">
+
+        <a class="navbar-brand" href="/">
+            Smart Canteen
+        </a>
+
+        <button class="navbar-toggler"
+                type="button"
+                data-bs-toggle="collapse"
+                data-bs-target="#navbarNav">
+
+            <span class="navbar-toggler-icon"></span>
+
+        </button>
+
+        <div class="collapse navbar-collapse" id="navbarNav">
+
+            <ul class="navbar-nav ms-auto">
+
+                <li class="nav-item">
+                    <a class="nav-link" href="/">Home</a>
+                </li>
+
+                <li class="nav-item">
+                    <a class="nav-link" href="/menu">Menu</a>
+                </li>
+
+                <li class="nav-item">
+                    <a class="nav-link" href="/cart">Cart</a>
+                </li>
+
+                <li class="nav-item">
+                    <a class="nav-link" href="/my-orders">My Orders</a>
+                </li>
+
+                <li class="nav-item">
+                    <a class="nav-link" href="/login">Login</a>
+                </li>
+
+                <li class="nav-item">
+                    <a class="nav-link" href="/register">Register</a>
+                </li>
+
+            </ul>
+
+        </div>
+
+    </div>
+</nav>
