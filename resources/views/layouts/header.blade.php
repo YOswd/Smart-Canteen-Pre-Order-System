19cc02ddce49de8625c7288a10fa362cc5531e1c
Riti <your_email@example.com>
2026-07-11 09:51:58 +0600

    changed header code

--- resources/views/layouts/header.blade.php
+++ resources/views/layouts/header.blade.php
@@ -1,50 +1,71 @@
-<nav class="navbar navbar-expand-lg navbar-dark bg-success">
+@php
+use Illuminate\Support\Facades\Auth;
+@endphp
+<nav class="navbar navbar-expand-lg navbar-dark bg-success shadow">
     <div class="container">
 
-        <a class="navbar-brand" href="/">
-            Smart Canteen
+        <a class="navbar-brand fw-bold" href="/">
+            🍽️ Smart Canteen
         </a>
 
-        <button class="navbar-toggler"
-                type="button"
+        <button class="navbar-toggler" type="button"
                 data-bs-toggle="collapse"
-                data-bs-target="#navbarNav">
+                data-bs-target="#navbarNav"
+                aria-controls="navbarNav"
+                aria-expanded="false"
+                aria-label="Toggle navigation">
 
             <span class="navbar-toggler-icon"></span>
 
         </button>
 
-        <div class="collapse navbar-collapse" id="navbarNav">
+        <div class="navbar-collapse d-flex justify-content-end">
+        <ul class="navbar-nav ms-auto">
 
-            <ul class="navbar-nav ms-auto">
+        <li class="nav-item">
+            <a class="nav-link" href="/">Home</a>
+        </li>
 
-                <li class="nav-item">
-                    <a class="nav-link" href="/">Home</a>
-                </li>
+        <li class="nav-item">
+            <a class="nav-link" href="/menu">Menu</a>
+        </li>
 
-                <li class="nav-item">
-                    <a class="nav-link" href="/menu">Menu</a>
-                </li>
+        @guest
+        <li class="nav-item">
+            <a class="nav-link" href="{{ route('login') }}">Login</a>
+        </li>
 
-                <li class="nav-item">
-                    <a class="nav-link" href="/cart">Cart</a>
-                </li>
+        <li class="nav-item">
+            <a class="nav-link" href="{{ route('register') }}">Register</a>
+        </li>
+        @endguest
 
-                <li class="nav-item">
-                    <a class="nav-link" href="/my-orders">My Orders</a>
-                </li>
+        @auth
+        <li class="nav-item">
+            <a class="nav-link" href="/cart">Cart</a>
+        </li>
 
-                <li class="nav-item">
-                    <a class="nav-link" href="/login">Login</a>
-                </li>
+        <li class="nav-item">
+            <a class="nav-link" href="/my-orders">My Orders</a>
+        </li>
 
-                <li class="nav-item">
-                    <a class="nav-link" href="/register">Register</a>
-                </li>
+        <li class="nav-item">
+            <span class="nav-link">
+                Welcome, {{ Auth::user()->name }}
+            </span>
+        </li>
 
-            </ul>
+        <li class="nav-item">
+            <form action="{{ route('logout') }}" method="POST">
+                @csrf
+                <button class="btn btn-outline-light ms-2">
+                    Logout
+                </button>
+            </form>
+        </li>
+        @endauth
 
+        </ul>
         </div>
-
     </div>
 </nav>
